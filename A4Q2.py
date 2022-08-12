a89fd4edb766deda4cf36a489541f948e2f58bc3
PranitMandaokar <109650592+PranitMandaokar@users.noreply.github.com>
2022-08-12 20:20:00 +0530

    Delete A4Q2.py

--- A4Q2.py
+++
@@ -1,9 +0,0 @@
-# map(function(element,index,array)){}, this)
-
-sample_list = [1,2,3,4,5,6,7]
-
-print(f"Sample list : {sample_list}")
-
-result = map(lambda x : x * 3 , sample_list)
-
-print(f'Triple of list numbers : {list(result)}')
