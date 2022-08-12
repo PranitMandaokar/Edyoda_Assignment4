e61d647972b234462e0ca10c137c13548882f1d6
PranitMandaokar <109650592+PranitMandaokar@users.noreply.github.com>
2022-08-12 20:20:35 +0530

    Delete A4Q1.py

--- A4Q1.py
+++
@@ -1,8 +0,0 @@
-# To write a python program to create a lambda function that adds 25 to a given number passed in as an argument :
-
-a = int(input("Enter the number : "))
-
-x = (lambda a : a + 25)
-
-print(x(a))
-
