dc316008699cc54a49f5b037f390283bf6c81c69
PranitMandaokar <109650592+PranitMandaokar@users.noreply.github.com>
2022-08-12 20:20:19 +0530

    Delete A4Q3.py

--- A4Q3.py
+++
@@ -1,9 +0,0 @@
-# Python program to square the elements of a list using map() function.
-
-a = [4,5,2,9]
-
-print(f'list : {a}')
-
-result = map(lambda x : x ** 2 , a )
-
-print(f'square list : {list(result)}')
